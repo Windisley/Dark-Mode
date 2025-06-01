ae17b2ec101be28b05254568986abcd3fb8ff543
Windisley <windisleyl@gmail.com>
2025-06-01 20:40:32 -0300

    Logica Para O Circulo E Para O Fim E Inicio

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ const boxLigthOf = document.querySelector(".box_ligth_of")
 const ligthOf = document.querySelector(".ligth_of")
 
 boxLigthOf.addEventListener("click", ()=>{
-    
+    ligthOf.classList.toggle("ligth_on")
+
 })
