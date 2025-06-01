4ea3ce6f3131fcda6f800b55176d6768a98c4749
Windisley <windisleyl@gmail.com>
2025-06-01 20:55:14 -0300

    Projeto Dark Mode Pronto

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const boxLigthOf = document.querySelector(".box_ligth_of")
 const ligthOf = document.querySelector(".ligth_of")
 const contDarkMode = document.querySelector(".cont_dark_mode_of")
+const containerOn = document.querySelector(".container")
 
 boxLigthOf.addEventListener("click", ()=>{
     ligthOf.classList.toggle("ligth_on")
     contDarkMode.classList.toggle("cont_dark_mode_on")
+    containerOn.classList.toggle("container_on")
 
 })
