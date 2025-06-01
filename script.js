a7a3a41c630d8e3006f8b73380ebf8b528c14b12
Windisley <windisleyl@gmail.com>
2025-06-01 20:45:14 -0300

    Logica Para A Troca De Imagens Do Dark Mode Pronta

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const boxLigthOf = document.querySelector(".box_ligth_of")
 const ligthOf = document.querySelector(".ligth_of")
+const contDarkMode = document.querySelector(".cont_dark_mode_of")
 
 boxLigthOf.addEventListener("click", ()=>{
     ligthOf.classList.toggle("ligth_on")
+    contDarkMode.classList.toggle("cont_dark_mode_on")
 
 })
